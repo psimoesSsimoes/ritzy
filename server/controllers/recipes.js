5278c7f01d7e26875fd0065324d6d8e3edc9705f
pedro <p.simoes@campus.fct.unl.pt>
2017-03-27 15:46:23 +0100

    small export from the saborintenso database for testing

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -3,7 +3,6 @@ var Models = require('../models');
 
 module.exports = {
     all: function(req, res, next) {
-
         Models.Recipe.findAll({}, function(err, products) {
             if (err) throw err;
             if (products.length > 1) {
@@ -15,7 +14,10 @@ module.exports = {
                 res.writeHead(204); // unauthorized
                 res.end();
             }
-
         });
+    },
+    category: function(req, res, next) {
+
+        res.send(200, req.params.category);
     }
 }
